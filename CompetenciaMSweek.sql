fa3bb087f42695a561c4a529e85f42a927bf8c56
Joseph Szot <38385215+jsphszot@users.noreply.github.com>
2020-01-22 23:02:47 -0500

    added explanatory comments

--- CompetenciaMSweek.sql
+++ CompetenciaMSweek.sql
@@ -1,12 +1,14 @@
--- SELECT DISTINCT ZonaOrigenAWB FROM `ReporteWeek.MercadoBASE`
-DECLARE Week0 INT64 DEFAULT 2;
+/*
+Takes Market info for Cargo tons, 
+*/
+DECLARE Week0 INT64 DEFAULT 2; -- current week number, for testing just wrote 2
 
 SELECT
 CONCAT(CAST(ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY sum(W) DESC) AS STRING), Grupo) AS KEY,
 Grupo,
 OwnerGrpd,
--- ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY sum(W) DESC) AS Rank,
-IEEE_DIVIDE(sum(W), sum(sum(W)) OVER (PARTITION BY Grupo)) AS Wr,
+-- ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY sum(W) DESC) AS Rank, -- put this into KEY (used for vlookup in excel)
+IEEE_DIVIDE(sum(W), sum(sum(W)) OVER (PARTITION BY Grupo)) AS Wr, -- assigns record to NULL if zero division
 IEEE_DIVIDE(sum(Wm1), sum(sum(Wm1)) OVER (PARTITION BY Grupo)) AS Wm1r,
 IEEE_DIVIDE(sum(Wm2), sum(sum(Wm2)) OVER (PARTITION BY Grupo)) AS Wm2r,
 IEEE_DIVIDE(sum(Wm3), sum(sum(Wm3)) OVER (PARTITION BY Grupo)) AS Wm3r,
@@ -20,8 +22,11 @@ FROM (
     SELECT 
     *,
     CASE 
-    WHEN Owner = 'LA' Then Owner
-    WHEN Grupo NOT LIKE 'ASIA%' AND ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY W DESC) > 5 Then 'Otros'
+    -- if LA is in the market, show MS no matter how low it is
+    WHEN Owner = 'LA' Then Owner 
+    -- only top 5 (plus LA if not in top), rest goes in others
+    WHEN Grupo NOT LIKE 'ASIA%' AND ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY W DESC) > 5 Then 'Otros' 
+    -- if Asian origin markets, show top 10 (plus LA if not in top), rest goes in others (diluted market amongst many competitors)
     WHEN ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY W DESC) > 10 Then 'Otros'
     ELSE Owner
     END AS OwnerGrpd
@@ -32,18 +37,19 @@ FROM (
         ELSE CONCAT(Origen, ' - ', Destino)
         END AS Grupo,
         Owner,
+        -- due to two week lag for data from Brasil make each Week (W, WmX) column equal to current week minus 2 for GRU and VCP destinations.
         sum(IF ((RelWeek=week0-0-2 AND Destino IN ('VCP', 'GRU')) OR (Destino NOT IN ('VCP', 'GRU') AND RelWeek=week0-0), Tons, 0)) AS W,
         sum(IF ((RelWeek=week0-1-2 AND Destino IN ('VCP', 'GRU')) OR (Destino NOT IN ('VCP', 'GRU') AND RelWeek=week0-1), Tons, 0)) AS Wm1,
         sum(IF ((RelWeek=week0-2-2 AND Destino IN ('VCP', 'GRU')) OR (Destino NOT IN ('VCP', 'GRU') AND RelWeek=week0-2), Tons, 0)) AS Wm2,
         sum(IF ((RelWeek=week0-3-2 AND Destino IN ('VCP', 'GRU')) OR (Destino NOT IN ('VCP', 'GRU') AND RelWeek=week0-3), Tons, 0)) AS Wm3,
         sum(IF ((RelWeek=week0-4-2 AND Destino IN ('VCP', 'GRU')) OR (Destino NOT IN ('VCP', 'GRU') AND RelWeek=week0-4), Tons, 0)) AS Wm4,
-        -- sum(Tons) as Tons
         FROM (
             SELECT 
             Year,
             Semana,
             RelWeek,
-            CASE 
+            -- define groups according to different origin-destination concepts
+            CASE
                 WHEN ZonaOrigenAWB = 'NA_USA_Florida' Then 'FL'
                 WHEN ZonaOrigenAWB IN ('NA_Canada', 'NA_USA_Midwest', 'NA_USA_NorthEast', 'NA_USA_South', 'NA_USA_SouthEast') Then 'GSA NA'
                 WHEN ZonaOrigenAWB = 'NA_USA_NewYork' Then 'JFK'
@@ -63,6 +69,7 @@ FROM (
                 WHEN RegionOrigenAWB = 'Europe' Then 'OTROS EU'
                 ELSE 'Otros' 
             END AS Origen,
+            -- main destinations for SouthBound flights, Chile and Brasil (GRU, VCP)
             CASE
                 WHEN PaisDestinoAWB = 'CHILE' Then 'CL'
                 WHEN PostaDestinoAWB IN ('GRU', 'VCP') Then PostaDestinoAWB
@@ -73,7 +80,7 @@ FROM (
             Tons
             FROM `ReporteWeek.McdoBASE`
         ) cookie
-        WHERE Origen != 'Otros' AND Destino != 'Otros'
+        WHERE Origen != 'Otros' AND Destino != 'Otros' -- drop all unimportant groups
         GROUP BY 1,2
         ORDER BY 1,3 DESC
     ) cookie2
