e0da518e030519fbd0ac64f17f299b78432183c9
Joseph Szot <38385215+jsphszot@users.noreply.github.com>
2020-04-17 04:30:48 -0400

    Update CompMSweek-GTWY.sql

--- CompMSweek-GTWY.sql
+++ CompMSweek-GTWY.sql
@@ -1,10 +1,8 @@
 /*
-Gateway MarketShare: 
-Takes Market info for Cargo tons, calculates weekly MarketShare for LA and 
-top competitors per Origin-Destination group.
+Gateway MarketShare: Takes Market info for Cargo tons, calculates weekly MarketShare for LA and top competitors per Origin-Destination group.
 */
 
--- WeeklyReport shows info from currentweek-back_weeks (python input)
+-- WeeklyReport shows info from currentweek- back_weeks (python input)
 DECLARE Week0 INT64 DEFAULT EXTRACT(ISOWEEK FROM CURRENT_DATE())-{back_weeks};
 -- How many competitors to show per Origins (LA, +4 if NotAsia | + 9 if Asia, others), threshold
 DECLARE CompShow INT64 DEFAULT 5;
@@ -13,7 +11,9 @@ DECLARE CompShow INT64 DEFAULT 5;
 WITH cookie2 AS (
     SELECT 
     CASE 
-        WHEN Origen = 'USA' AND Destino = 'CHILE' THEN CONCAT(Origen, ' - ', Destino, " ", TipoVuelo)
+        -- covid has TipoVuelo excluded 
+        WHEN Destino = 'CHILE' THEN CONCAT(Origen, ' - ', Destino, " ", TipoVuelo)
+        WHEN Origen = 'USA' AND Destino IN ('LIM') THEN CONCAT(Origen, ' - ', Destino, " ", TipoVuelo)
         ELSE CONCAT(Origen, ' - ', Destino)
     END AS Grupo,
     Owner,
@@ -50,6 +50,30 @@ WITH cookie2 AS (
     WHERE Origen != 'Otros' AND Destino != 'Otros'
     GROUP BY 1,2
     ORDER BY 1,3 DESC
+),
+/*
+Here we split cookie2 into two tables: 
+    1. cookie3LA: filter Owner='LA', will later be given Rank0 (LA to appear first)
+    2. cookie3: all the other Owners, later will be split in two as well (OwnerGrpd 'Otros' vs the rest, as 'Otros' always wants to be shown last)
+
+This id done to always show the 'LA' and 'Otros' groupings in our final table no matter their MarketShare
+*/
+cookie3 AS (
+    SELECT 
+    *,
+    CASE
+        WHEN ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY W DESC) >= CompShow Then 'Otros'
+        ELSE Owner
+    END AS OwnerGrpd
+    FROM cookie2
+    WHERE Owner != 'LA'
+),
+cookie3LA AS (
+    SELECT 
+    *,
+    Owner AS OwnerGrpd
+    FROM cookie2
+    WHERE Owner = 'LA'
 )
 
 SELECT
@@ -71,11 +95,10 @@ Wm4
 FROM (
 
     /*
-    Here we split cookie2 into two tables: one where Owner='LA', and all the other Owner in the second table.
-    Remember we always want to show the LA grouping in our final table no matter how low the MarketShare
-    LA is ranked as 0, All other owners are ranked according to last week's tons (W) per Grupo and tagged as 'Otros' if 
+    LA is ranked as 0, Otros as 99, all other owners are ranked according to last week's tons (W) per Grupo and tagged as 'Otros' if 
     ranked below threshold. Both tables are then appended to each other.
     */
+    
     SELECT 
     ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY sum(W) DESC) AS Rank,
     Grupo,
@@ -85,16 +108,24 @@ FROM (
     sum(Wm2) as Wm2,
     sum(Wm3) as Wm3,
     sum(Wm4) as Wm4
-    FROM (
-        SELECT 
-        *,
-        CASE
-            WHEN ROW_NUMBER() OVER (PARTITION BY Grupo ORDER BY W DESC) >= CompShow Then 'Otros'
-            ELSE Owner
-        END AS OwnerGrpd
-        FROM cookie2
-        WHERE Owner != 'LA'
-        ) AS cookie3
+    FROM cookie3
+    WHERE OwnerGrpd != 'Otros'
+    GROUP BY 2,3
+
+    UNION ALL
+    
+    -- Otros assign rank to 99 (always last in list yet included)  
+    SELECT 
+    99 AS Rank,
+    Grupo,
+    OwnerGrpd,
+    sum(W) as W,
+    sum(Wm1) as Wm1,
+    sum(Wm2) as Wm2,
+    sum(Wm3) as Wm3,
+    sum(Wm4) as Wm4
+    FROM cookie3
+    WHERE OwnerGrpd = 'Otros'
     GROUP BY 2,3
 
     UNION ALL
@@ -108,13 +139,7 @@ FROM (
     sum(Wm2) as Wm2,
     sum(Wm3) as Wm3,
     sum(Wm4) as Wm4
-    FROM (
-        SELECT 
-        *,
-        Owner AS OwnerGrpd
-        FROM cookie2
-        WHERE Owner = 'LA'
-        ) AS cookie3LA
+    FROM cookie3LA
     GROUP BY 2,3
 
 ) FinalCookie
